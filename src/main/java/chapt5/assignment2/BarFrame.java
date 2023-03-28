a4b76578a947127c9e40fc6e456af58400c9e1c1
Eometheous <jonathan.sthomas@icloud.com>
2023-03-28 16:50:28 -0700

    BarFrame now can be updated by clicking with a mouse

---
+++ src/main/java/chapt5/assignment2/BarFrame.java
@@ -0,0 +1,123 @@
+package chapt5.assignment2;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.util.*;
+
+/**
+ A class that implements an Observer object that displays a barchart view of
+ a data model.
+ */
+public class BarFrame extends JFrame implements ChangeListener
+{
+    /**
+     Constructs a BarFrame object
+     @param dataModel the data that is displayed in the barchart
+     */
+    public BarFrame(DataModel dataModel)
+    {
+        this.dataModel = dataModel;
+        a = dataModel.getData();
+
+        setLocation(0,200);
+        setLayout(new BorderLayout());
+
+        Icon barIcon = new Icon()
+        {
+            public int getIconWidth() { return ICON_WIDTH; }
+            public int getIconHeight() { return ICON_HEIGHT; }
+            public void paintIcon(Component c, Graphics g, int x, int y)
+            {
+                Graphics2D g2 = (Graphics2D) g;
+
+                g2.setColor(Color.red);
+
+                double max = getMax();
+
+                double barHeight = (double) getIconHeight() / a.size(); // edited by Jonathan Stewart Thomas
+
+                int i = 0;
+                for (Double value : a) // edited by Jonathan Stewart Thomas
+                {
+                    double barLength = getIconWidth() * value / max;
+
+                    Rectangle2D.Double rectangle = new Rectangle2D.Double
+                            (0, barHeight * i, barLength, barHeight);
+                    i++;
+                    g2.fill(rectangle);
+                }
+            }
+        };
+
+        MouseListener mouseListener = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() - 8;
+                int y = e.getY() - 30;
+                int barHeight = barIcon.getIconHeight() / a.size();
+                int clickedBar = Math.floorDiv(y, barHeight);
+                double value;
+                double max = getMax();
+                value = (x / (double) barIcon.getIconWidth()) * max;
+                dataModel.update(clickedBar, value);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
+
+        addMouseListener(mouseListener);
+
+        add(new JLabel(barIcon));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+    // extracted by Jonathan Stewart Thomas
+    private double getMax() {
+        double max = a.get(0); // edited by Jonathan Stewart Thomas
+        for (Double v : a)
+        {
+            double val = v; // edited by Jonathan Stewart Thomas
+            if (val > max)
+                max = val;
+        }
+        return max;
+    }
+
+    /**
+     Called when the data in the model is changed.
+     @param e the event representing the change
+     */
+    public void stateChanged(ChangeEvent e)
+    {
+        a = dataModel.getData();
+        repaint();
+    }
+
+    private ArrayList<Double> a;
+    private final DataModel dataModel; // edited by Jonathan Stewart Thomas
+
+    private static final int ICON_WIDTH = 200;
+    private static final int ICON_HEIGHT = 200;
+}
---
+++ src/main/java/chapt5/assignment2/ObserverTester.java
@@ -0,0 +1,34 @@
+package chapt5.assignment2;
+
+import java.util.ArrayList;
+
+/**
+ A class for testing an implementation of the Observer pattern.
+ */
+public class ObserverTester
+{
+    /**
+     Creates a DataModel and attaches barchart and textfield listeners
+     @param args unused
+     */
+    public static void main(String[] args)
+    {
+        ArrayList<Double> data = new ArrayList<>();
+
+        // edited by Jonathan Stewart Thomas to get rid of deprecated code
+        data.add(33.0);
+        data.add(44.0);
+        data.add(22.0);
+        data.add(22.0);
+
+        DataModel model = new DataModel(data);
+
+        TextFrame frame = new TextFrame(model);
+        frame.setLocationRelativeTo(null); // added by Jonathan Stewart Thomas
+        BarFrame barFrame = new BarFrame(model);
+        barFrame.setLocation(frame.getX() + frame.getWidth(), frame.getY()); // added by Jonathan Stewart Thomas
+
+        model.attach(barFrame);
+        model.attach(frame);
+    }
+}
